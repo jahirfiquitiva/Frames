c12f8d86037a87c70be480e12c21bf3a7288e24d
jahirfiquitiva <jahir.fiquitiva@gmail.com>
2018-01-17 23:48:03 -0500

    Test

--- publish_apk.sh
+++ publish_apk.sh
@@ -1,14 +1,18 @@
-if [ -z "$TRAVIS_TAG" ]; then
-	cd $TRAVIS_BUILD_DIR/app/build/outputs/apk/release/
+BRANCH="master"
 
-	echo "Publishing APK to release"
-	for apk in $(find *.apk -type f); do
-	  apkName="${apk::-4}"
-	  printf "Found APK: $apkName\n"
-	  curl "https://uploads.github.com/repos/${TRAVIS_REPO_SLUG}/releases/${TRAVIS_TAG}/assets?access_token=${GITHUB_API_KEY}&name=${apkName}.apk" --header 'Content-Type: application/zip' --upload-file $apkName.apk -X POST
-	done
+if [ "$TRAVIS_BRANCH" = "$BRANCH" ]; then
+	if [ -z "$TRAVIS_TAG" ]; then
+		cd $TRAVIS_BUILD_DIR/app/build/outputs/apk/release/
 
-	echo -e "Done publishing APK\n"
-else
-	echo "Skipping APK publish because this commit does not have a tag"
+		echo "Publishing APK to release"
+		for apk in $(find *.apk -type f); do
+		  apkName="${apk::-4}"
+		  printf "Found APK: $apkName\n"
+		  curl "https://uploads.github.com/repos/${TRAVIS_REPO_SLUG}/releases/${TRAVIS_TAG}/assets?access_token=${GITHUB_API_KEY}&name=${apkName}.apk" --header 'Content-Type: application/zip' --upload-file $apkName.apk -X POST
+		done
+
+		echo -e "Done publishing APK\n"
+	else
+		echo "Skipping APK publish because this commit does not have a tag"
+	fi
 fi
