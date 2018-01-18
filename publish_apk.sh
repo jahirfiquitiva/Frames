6177b204a9db989810ff8811fbc884be134b5d62
jahirfiquitiva <jahir.fiquitiva@gmail.com>
2018-01-17 23:46:35 -0500

    Test

---
+++ publish_apk.sh
@@ -0,0 +1,14 @@
+if [ -z "$TRAVIS_TAG" ]; then
+	cd $TRAVIS_BUILD_DIR/app/build/outputs/apk/release/
+
+	echo "Publishing APK to release"
+	for apk in $(find *.apk -type f); do
+	  apkName="${apk::-4}"
+	  printf "Found APK: $apkName\n"
+	  curl "https://uploads.github.com/repos/${TRAVIS_REPO_SLUG}/releases/${TRAVIS_TAG}/assets?access_token=${GITHUB_API_KEY}&name=${apkName}.apk" --header 'Content-Type: application/zip' --upload-file $apkName.apk -X POST
+	done
+
+	echo -e "Done publishing APK\n"
+else
+	echo "Skipping APK publish because this commit does not have a tag"
+fi
