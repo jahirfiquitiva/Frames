9366166a4c901d7d83aea26d4ccb6b707582d4d3
Jahir Fiquitiva <jahir.fiquitiva@gmail.com>
2018-12-23 12:20:28 -0500

    Add support for Webp images

--- library/src/main/kotlin/jahirfiquitiva/libs/frames/helpers/glide/Extensions.kt
+++ library/src/main/kotlin/jahirfiquitiva/libs/frames/helpers/glide/Extensions.kt
@@ -22,7 +22,11 @@ import com.bumptech.glide.Glide
 import com.bumptech.glide.Priority
 import com.bumptech.glide.RequestBuilder
 import com.bumptech.glide.RequestManager
+import com.bumptech.glide.integration.webp.decoder.WebpDrawable
+import com.bumptech.glide.integration.webp.decoder.WebpDrawableTransformation
 import com.bumptech.glide.load.DecodeFormat
+import com.bumptech.glide.load.resource.bitmap.CircleCrop
+import com.bumptech.glide.load.resource.bitmap.FitCenter
 import com.bumptech.glide.load.resource.drawable.DrawableTransitionOptions
 import com.bumptech.glide.request.RequestListener
 import com.bumptech.glide.request.RequestOptions
@@ -56,6 +60,13 @@ fun RequestManager.loadPicture(
         
         if (fitCenter) options = options.optionalFitCenter()
         if (circular) options = options.optionalCircleCrop()
+        
+        if (url.endsWith("webp", true) || thumbnail.endsWith("webp", true)) {
+            options = options.optionalTransform(
+                WebpDrawable::class.java,
+                WebpDrawableTransformation(
+                    if (fitCenter) FitCenter() else if (circular) CircleCrop() else null))
+        }
         
         val transition = if (context == null || context.isLowRamDevice) {
             DrawableTransitionOptions.with { _, _ -> NoTransition<Drawable>() }
