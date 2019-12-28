8a4e593355a9f307163184910b68eaf8d6c757fb
Jahir Fiquitiva <jahir.fiquitiva@gmail.com>
2019-12-27 20:27:55 -0500

    Add string extensions

--- library/src/main/kotlin/dev/jahir/frames/extensions/String.kt
+++ library/src/main/kotlin/dev/jahir/frames/extensions/String.kt
@@ -1,3 +1,9 @@
 package dev.jahir.frames.extensions
 
+import java.util.*
+
 fun String.hasContent(): Boolean = isNotBlank() && isNotEmpty()
+
+fun String.lower(locale: Locale = Locale.ROOT) = toLowerCase(locale)
+
+fun String.upper(locale: Locale = Locale.ROOT) = toUpperCase(locale)
