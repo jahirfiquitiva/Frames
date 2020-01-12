48df59b8f51157b7975cb8c543ccd3f82dd909c9
Jahir Fiquitiva <jahir.fiquitiva@gmail.com>
2020-01-12 14:06:33 -0500

    Show changelog when option is pressed

--- library/src/main/kotlin/dev/jahir/frames/ui/activities/base/BaseChangelogDialogActivity.kt
+++ library/src/main/kotlin/dev/jahir/frames/ui/activities/base/BaseChangelogDialogActivity.kt
@@ -1,6 +1,8 @@
 package dev.jahir.frames.ui.activities.base
 
+import android.view.MenuItem
 import androidx.appcompat.app.AlertDialog
+import dev.jahir.frames.R
 import dev.jahir.frames.extensions.isUpdate
 import dev.jahir.frames.utils.Prefs
 import dev.jahir.frames.utils.buildChangelogDialog
@@ -11,6 +13,11 @@ abstract class BaseChangelogDialogActivity<out P : Prefs> : BaseSearchableActivi
 
     internal fun showChangelog(force: Boolean = false) {
         if (isUpdate || force) changelogDialog?.show()
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (item.itemId == R.id.changelog) showChangelog(true)
+        return super.onOptionsItemSelected(item)
     }
 
     override fun onDestroy() {
