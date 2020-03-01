9eaa619bc8ce552b7d5c14d3b5e8b3018989d66a
Jahir Fiquitiva <jahir.fiquitiva@gmail.com>
2020-03-01 15:38:14 -0500

    Upgrade lib version

--- buildSrc/src/main/java/Frames.kt
+++ buildSrc/src/main/java/Frames.kt
@@ -2,6 +2,6 @@
 
 object Frames {
     const val appId = "dev.jahir.frames.app"
-    const val version = 300
-    const val versionName = "3.0.0"
+    const val version = 301
+    const val versionName = "3.0.1"
 }
