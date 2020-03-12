c9fb594ca07b0deca64cbe43b5f6d8a8c8e078a4
Jahir Fiquitiva <jahir.fiquitiva@gmail.com>
2020-03-11 21:23:08 -0500

    Minor fix

--- library/src/main/kotlin/dev/jahir/frames/extensions/BillingLibrary.kt
+++ library/src/main/kotlin/dev/jahir/frames/extensions/BillingLibrary.kt
@@ -2,16 +2,8 @@ package dev.jahir.frames.extensions
 
 import com.android.billingclient.api.Purchase
 import com.android.billingclient.api.PurchaseHistoryRecord
-import com.android.billingclient.api.SkuDetails
 import com.google.gson.Gson
-import dev.jahir.frames.data.models.CleanSkuDetails
 import dev.jahir.frames.data.models.DetailedPurchaseRecord
-
-fun SkuDetails.asCleanSkuDetails(): CleanSkuDetails? = try {
-    CleanSkuDetails(sku, title, description, price, type)
-} catch (e: Exception) {
-    null
-}
 
 fun Purchase.asDetailedPurchase(): DetailedPurchaseRecord? = try {
     Gson().fromJson(originalJson, DetailedPurchaseRecord::class.java)
