d418366004d2ac619f3300ddf912f8db7cd5f65b
Jahir Fiquitiva <jahir.fiquitiva@gmail.com>
2020-03-23 15:14:08 -0500

    Update dependencies

--- buildSrc/src/main/java/Versions.kt
+++ buildSrc/src/main/java/Versions.kt
@@ -3,7 +3,7 @@
 object Versions {
     // Plugins
     const val gradle = "3.6.1"
-    const val kotlin = "1.3.70"
+    const val kotlin = "1.3.71"
     const val jitpack = "2.1"
 
     // OneSignal
@@ -16,7 +16,7 @@ object Versions {
     const val buildTools = "29.0.3"
 
     // Coroutines
-    const val coroutines = "1.3.4"
+    const val coroutines = "1.3.5"
 
     // Android UI
     const val appcompat = "1.1.0"
@@ -31,11 +31,11 @@ object Versions {
     const val lifecycle = "2.2.0"
 
     // Room Database
-    const val room = "2.2.4"
+    const val room = "2.2.5"
 
     // Network & Serialization
     const val gson = "2.8.6"
-    const val retrofit = "2.7.2"
+    const val retrofit = "2.8.0"
 
     // Image loading and Photo View
     const val coil = "0.9.5"
