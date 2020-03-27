4591b752fd5b9b116b3dde06f24dee45f48c7f8b
Jahir Fiquitiva <jahir.fiquitiva@gmail.com>
2020-03-27 13:56:18 -0500

    Minor fixes

--- library/src/main/kotlin/dev/jahir/frames/extensions/String.kt
+++ library/src/main/kotlin/dev/jahir/frames/extensions/String.kt
@@ -2,8 +2,8 @@ package dev.jahir.frames.extensions
 
 import java.util.*
 
-fun String.hasContent(): Boolean = isNotBlank() && isNotEmpty()
+fun String?.hasContent(): Boolean = orEmpty().isNotBlank() && orEmpty().isNotEmpty()
 
-fun String.lower(locale: Locale = Locale.ROOT) = toLowerCase(locale)
+fun String?.lower(locale: Locale = Locale.ROOT): String? = orEmpty().toLowerCase(locale)
 
-fun String.upper(locale: Locale = Locale.ROOT) = toUpperCase(locale)
+fun String?.upper(locale: Locale = Locale.ROOT): String? = orEmpty().toUpperCase(locale)
