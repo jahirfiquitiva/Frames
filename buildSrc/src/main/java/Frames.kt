e6e55332ba2c54680ffc0781694ebf9a659a5ad5
Jahir Fiquitiva <jahir.fiquitiva@gmail.com>
2020-04-10 19:13:03 -0500

    Fix issue with buttons styles

--- buildSrc/src/main/java/Frames.kt
+++ buildSrc/src/main/java/Frames.kt
@@ -2,6 +2,6 @@
 
 object Frames {
     const val appId = "dev.jahir.frames.app"
-    const val version = 317
-    const val versionName = "3.1.7"
+    const val version = 318
+    const val versionName = "3.1.8"
 }
