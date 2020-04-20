98fbd392089ccf698eae5789fe6a4a15b1c3c2de
Jahir Fiquitiva <jahir.fiquitiva@gmail.com>
2020-04-20 12:09:06 -0500

    Update TouchImageView

--- buildSrc/src/main/java/Versions.kt
+++ buildSrc/src/main/java/Versions.kt
@@ -39,7 +39,7 @@ object Versions {
 
     // Image loading and Touch Image View
     const val coil = "0.9.5"
-    const val touchImageView = "3.0.0"
+    const val touchImageView = "3.0.1"
 
     // Harmonic Colors
     const val harmonicColors = "1.0.0"
