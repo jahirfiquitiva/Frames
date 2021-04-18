54cbdf919df3b9796bba887301d8b4b723f319b8
Jahir Fiquitiva <jahir.fiquitiva@gmail.com>
2021-04-18 16:32:22 -0500

    Update to Frames 3.3.8

--- buildSrc/src/main/java/MyApp.kt
+++ buildSrc/src/main/java/MyApp.kt
@@ -2,6 +2,6 @@
 
 object MyApp {
     const val appId = "dev.jahir.frames.app"
-    const val version = 337
-    const val versionName = "3.3.7"
+    const val version = 338
+    const val versionName = "3.3.8"
 }
