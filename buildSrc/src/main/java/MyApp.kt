ccec13b848fb78b93b06eba404839aecebc9553e
Jahir Fiquitiva <jahir.fiquitiva@gmail.com>
2021-05-05 18:58:50 -0500

    Update MyApp.kt

--- buildSrc/src/main/java/MyApp.kt
+++ buildSrc/src/main/java/MyApp.kt
@@ -2,6 +2,6 @@
 
 object MyApp {
     const val appId = "dev.jahir.frames.app"
-    const val version = 341
-    const val versionName = "3.4.1"
+    const val version = 342
+    const val versionName = "3.4.2"
 }
