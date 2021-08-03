66d005b4af51b75b3ce26e9b44eee4075c93287e
Jahir Fiquitiva <jahir.fiquitiva@gmail.com>
2021-08-02 23:57:31 -0500

    Initial use of upload action

--- publish_apk.sh
+++ publish_apk.sh
@@ -30,38 +30,10 @@ cd ./app/build/outputs/apk/release/
     for apk in $(find *.apk -type f); do
       FILE="$apk"
 
+      echo "release_tag=$releaseName" >> $GITHUB_ENV
       echo "apk_file=$FILE" >> $GITHUB_ENV
+      echo "release_changes=$defaultChanges" >> $GITHUB_ENV
+      echo "second_changes=$changes" >> $GITHUB_ENV
 
-      printf "\n\nUploading: $FILE ... \n"
-      upload=$(
-        curl \
-          -H "Authorization: token $GITHUB_API_KEY" \
-          -H "Content-Type: $(file -b --mime-type $FILE)" \
-          --data-binary @$FILE \
-          --upload-file $FILE \
-          "https://uploads.github.com/repos/${GITHUB_REPOSITORY}/releases/${releaseId}/assets?name=$(basename $FILE)&access_token=${GITHUB_API_KEY}"
-      )
-
-      url="$(echo "$upload" | jq --compact-output ".browser_download_url")"
-      url=$(echo ${url} | cut -d "\"" -f 2)
-      url=$(echo "${url//\"\r\n\"/$ln}")
-      url=$(echo "${url//'\r\n'/$ln}")
-      url=$(echo "${url//\\r\\n/$ln}")
-
-      if [[ ! -z "$url" && "$url" != " " && "$url" != "null" ]]; then
-        printf "\nAPK url: $url"
-        printf "\n\nSending message to Discord channel…\n"
-        message="**Changes:**\n$defaultChanges"
-        message+="\n\n**Useful links:**"
-        message+="\n* [How to update?](https://github.com/jahirfiquitiva/$repoName/wiki/How-to-update)"
-        message+="\n* [Download sample APK]("
-        message+="$url"
-        message+=")\n* [Donate & support future development](https://jahir.dev/donate)"
-        curl -X POST -H 'Content-Type: application/json' -d '{ "embeds": [{ "title": "**New update available! ('"$releaseName"')** 🚀", "description": "'"$message"'", "color": 15844367 }] }' $UPDATE_DISCORD_WEBHOOK
-
-        printf "\n\nFinished uploading APK(s) and sending notifications\n"
-      else
-      	printf "\n\nUpload Result: $upload\n"
-        printf "\n\nSkipping notifications because no file was uploaded\n"
-      fi
+      break
     done
