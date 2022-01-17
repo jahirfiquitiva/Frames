b967f8c0946670de9f0e823b6f0f59cd940bf387
Jahir Fiquitiva <jahir.fiquitiva@gmail.com>
2022-01-17 18:03:18 -0500

    Update to Frames 3.4.7

--- buildSrc/src/main/java/Versions.kt
+++ buildSrc/src/main/java/Versions.kt
@@ -2,12 +2,12 @@
 
 object Versions {
     // Plugins
-    const val gradle = "7.0.3"
+    const val gradle = "7.0.4"
     const val kotlin = "1.5.31"
 
     // OneSignal
     const val oneSignalPlugin = "0.14.0"
-    const val oneSignal = "4.6.3"
+    const val oneSignal = "4.6.5"
 
     // App
     const val minSdk = 21
@@ -15,5 +15,5 @@ object Versions {
     const val buildTools = "30.0.2"
 
     // Frames
-    const val frames = "3.4.6"
+    const val frames = "3.4.7"
 }
