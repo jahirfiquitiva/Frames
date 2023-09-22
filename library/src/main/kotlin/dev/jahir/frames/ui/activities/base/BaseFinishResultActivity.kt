ee1c8d5fc8169fb48337e24b750d6b28fd3a3ca2
Jahir Fiquitiva <jahir.fiquitiva@gmail.com>
2023-09-22 12:40:30 -0500

    fix: support for the predictive back gesture

--- library/src/main/kotlin/dev/jahir/frames/ui/activities/base/BaseFinishResultActivity.kt
+++ library/src/main/kotlin/dev/jahir/frames/ui/activities/base/BaseFinishResultActivity.kt
@@ -1,18 +1,8 @@
 package dev.jahir.frames.ui.activities.base
 
-import android.os.Bundle
-import androidx.activity.addCallback
 import androidx.appcompat.app.AppCompatActivity
 
 abstract class BaseFinishResultActivity : AppCompatActivity() {
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        onBackPressedDispatcher.addCallback(this) {
-            onSafeBackPressed()
-        }
-    }
-
     override fun finish() {
         onFinish()
         super.finish()
