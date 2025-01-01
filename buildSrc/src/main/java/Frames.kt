d416c0e62bd8cc214227cea3c4b418adb4265620
Jahir Fiquitiva <jahir.fiquitiva@gmail.com>
2025-01-01 17:08:16 -0500

    chore: update Frames version

--- buildSrc/src/main/java/Frames.kt
+++ buildSrc/src/main/java/Frames.kt
@@ -2,6 +2,6 @@
 
 object Frames {
     const val appId = "dev.jahir.frames.app"
-    const val version = 362
-    const val versionName = "3.6.2"
+    const val version = 363
+    const val versionName = "3.6.3"
 }
