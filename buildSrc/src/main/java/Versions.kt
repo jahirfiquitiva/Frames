365e0c2cff2c5c6821aaa4ea5c8940acb64e3a1d
Jahir Fiquitiva <jahir.fiquitiva@gmail.com>
2025-01-01 13:52:43 -0500

    chore: upgrade dependencies

--- buildSrc/src/main/java/Versions.kt
+++ buildSrc/src/main/java/Versions.kt
@@ -2,21 +2,21 @@
 
 object Versions {
     // Plugins
-    const val gradle = "8.5.0"
-    const val kotlin = "2.0.0"
+    const val gradle = "8.6.1"
+    const val kotlin = "2.1.0"
     const val sonatype = "2.0.0"
-    const val ksp = "$kotlin-1.0.22"
+    const val ksp = "$kotlin-1.0.29"
 
     // OneSignal
     const val oneSignal = "4.8.10"
 
     // App
     const val minSdk = 21
-    const val targetSdk = 34
-    const val buildTools = "34.0.0"
+    const val targetSdk = 35
+    const val buildTools = "35.0.0"
 
     // Coroutines
-    const val coroutines = "1.8.1"
+    const val coroutines = "1.10.1"
 
     // Android UI
     const val appcompat = "1.7.0"
@@ -26,25 +26,25 @@ object Versions {
     const val palette = "1.0.0"
     const val preference = "1.2.1"
     const val materialComponents = "1.12.0"
-    const val activityKtx = "1.9.0"
-    const val fragmentKtx = "1.8.1"
+    const val activityKtx = "1.9.3"
+    const val fragmentKtx = "1.8.5"
 
     // ViewModel and LiveData
-    const val lifecycle = "2.8.2"
+    const val lifecycle = "2.8.7"
     const val lifecycleExt = "2.2.0"
 
     // Room Database
     const val room = "2.6.1"
 
     // Work Manager
-    const val work = "2.9.0"
+    const val work = "2.10.0"
 
     // Network & Serialization
     const val gson = "2.11.0"
     const val retrofit = "2.11.0"
 
     // Image loading and Touch Image View
-    const val coil = "2.6.0"
+    const val coil = "2.7.0"
     const val touchImageView = "3.6"
 
     // Harmonic Colors
@@ -54,7 +54,7 @@ object Versions {
     const val sectionedRecyclerView = "ea9a5cb"
 
     // FastScroll RecyclerView
-    const val fastScrollRecyclerView = "-SNAPSHOT"
+    const val fastScrollRecyclerView = "9463d46"
 
     // Permissions
     const val permissions = "3.5.0"
@@ -63,7 +63,7 @@ object Versions {
     const val licenseChecker = "1.2.8"
 
     // In-App Billing
-    const val inAppBilling = "7.0.0"
+    const val inAppBilling = "7.1.1"
 
     // MultiDex
     const val multidex = "2.0.1"
